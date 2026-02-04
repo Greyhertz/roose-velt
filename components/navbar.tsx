202c04fe015edad5c4f2ef268d71716a6a6e4f9a
Prince <greyhert120@gmail.com>
2026-02-04 15:47:00 +0100

    updated-components

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { Home, Sprout } from "lucide-react";
+import { ExternalLink, Home, LogIn, Sprout } from "lucide-react";
 import { Modetoggle } from "./mode-toggle";
+import { neonAuth } from "@neondatabase/auth/next/server";
 
 export function Navbar(){
+    const app = neonAuth.getUser();
     return(
         <div>
             <nav className="sticky top-0  border-b border-gray-700 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50 p-4">
@@ -32,7 +34,16 @@ export function Navbar(){
                         <span className="hidden lg:inline">Home</span>
                         </Link>
                     </Button>
+
                     <Modetoggle />
+
+                    {/*      */}
+                     <Button variant="ghost" className="flex items-center gap-2" asChild>
+                        <Link href={app.signIn}>
+                        <LogIn className="w-4 h-4"/>
+                        <span className="hidden lg:inline">Sign In</span>
+                        </Link>
+                    </Button>
                       </div>
                 </div>
                  </div>
